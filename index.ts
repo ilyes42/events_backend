d5610866b7afe09b481fd04f3559f715e939d1f3
ilyes <ilyes.hammemi@keyrus.com>
2022-08-05 10:28:23 +0100

    Add postgres datasource and configure DB connection

--- index.ts
+++ index.ts
@@ -1,32 +1,50 @@
 import "reflect-metadata";
-import { Event } from "./models/Event";
+import { Event } from "./entities/Event";
+import { DataSource } from "typeorm";
 import express, { Express, Request, Response, json } from "express";
 const app: Express = express();
 const port: number = 3000;
 
 app.use(json());
 
-const events: Event[] = [];
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "p@ssw0rd",
+  database: "events-app-db",
+  entities: [Event],
+  synchronize: true,
+  logging: false,
+});
 
 // GET All Events.
-app.get("/api/v1/events", (req: Request, res: Response) => {
-  return res.status(200).json(events);
+app.get("/api/v1/events", async (req: Request, res: Response) => {
+  const allEvents = await AppDataSource.manager.find(Event);
+  return res.status(200).json(allEvents);
 });
 
 // GET One Event By ID.
-app.get("/api/v1/events/:id", (req: Request, res: Response) => {
-  const eventId: number = parseInt(req.params.id);
-  const event = events.filter((e) => e.id === eventId);
-  res.status(200).json(event);
-});
+// app.get("/api/v1/events/:id", (req: Request, res: Response) => {
+//   const eventId: number = parseInt(req.params.id);
+//   const event = events.filter((e) => e.id === eventId);
+//   res.status(200).json(event);
+// });
 
-app.post("/api/v1/events", (req: Request, res: Response) => {
+app.post("/api/v1/events", async (req: Request, res: Response) => {
   const eventName: string = req.body.name;
   const newEvent: Event = new Event(eventName);
-  events.push(newEvent);
-  return res.status(201).json(events);
+  await AppDataSource.manager.save<Event>(newEvent);
+  const allEevents = await AppDataSource.manager.find(Event);
+  return res.status(201).json(allEevents);
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connection successful ! Yeah baby !");
+    app.listen(port, () => {
+      console.log(`Server successfully listening on port ${port} ! Oh yeah !`);
+    });
+  })
+  .catch((error) => console.log(error));
